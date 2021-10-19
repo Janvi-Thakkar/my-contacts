dcc5ed6a52f1f7d64ce096866818bc2c59323c18
Janvi <janvi@summachar.in>
2021-10-19 16:28:54 +0530

    updated readme file

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ This part of app contain frontend part which is in React. Another part contain M
 I have used Mongodb atlas as database. Express and Nodejs to write api and connection of backend server.
 
 This app helped me to learn CRUD operations in Mongodb.
+
+Features: - User will be able to create account where Mobile number, Name, Email and Password fields are mandatory.
+          - User will need to signin using their mobile number and password whenever he/she will need to login again once logged out. 
+          - Passwords are stored as hashed key in the database.
+          - User will only be able to login once he/she has entered right password. This will enable security and privacy in the app.
+          - User will be able to create new contacts with Mobile number, Email and Name fields.
+          - User can modify contact detail such as name or email.
+          - User will be able to delete contact  .
+
+Backend Repo Link: https://github.com/Janvi-Thakkar/my-contacts-api
+
+Hosted Backend Link:  https://my-contact-api-backend.herokuapp.com/api/
+
+Project Repo Link: https://github.com/Janvi-Thakkar/my-contacts
+
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -17,7 +17,8 @@ const SignInPage = (props) => {
 	return (
 		<div className="outer_container">
 		<div className="signin_container" id="container">
-				{(windowSize.width > 600 || !showSignIn) && <><div className="form-container sign-up-container">
+				{(windowSize.width > 600 || !showSignIn) &&
+					<><div className="form-container sign-up-container">
 					<form >
 						<h1>Create Account</h1>
 						<input type="text" placeholder="Name" onChange={(event) => { props.setRegisterDetails(event.target.value, "name") }} />
@@ -25,40 +26,48 @@ const SignInPage = (props) => {
 						<input type="email" placeholder="Email" onChange={(event) => { props.setRegisterDetails(event.target.value, "email") }} />
 						<input type="password" placeholder="Password" onChange={(event) => { props.setRegisterDetails(event.target.value, "pass") }} />
 						<button onClick={(event) => { props.signUp(event) }}>Sign Up</button>
-						{windowSize.width < 600 &&
+						{
+							windowSize.width < 600 &&
 							<span className="account-span">Already have an Account? <button className="clickLink" onClick={() => { show(true) }}>Sign In</button></span>
 
-						}</form>
-				</div>	  </>}
-				{(windowSize.width > 600 || showSignIn) && <><div className="form-container sign-in-container">
-				<form action="#">
+						}
+					</form>
+					</div>
+					</>
+				}
+				{(windowSize.width > 600 || showSignIn) &&
+					<><div className="form-container sign-in-container">
+			       	<form >
 						<h1>Sign In</h1>
 						
 						<input type="tel" placeholder="Mobile No." defaultValue={props.signInDetail.mobile} onChange={(event) => {props.setSignInDetails(event.target.value,"mobile")}}/>
 						<input type="password" placeholder="Password" defaultValue={props.signInDetail.password} onChange={(event) => { props.setSignInDetails(event.target.value, "pass") }}/>
 						<button onClick={(event) => { props.signIn(event, props.signInDetail.mobile, props.signInDetail.password)}}>Sign In</button>
-						{windowSize.width < 600 &&
+						{
+							windowSize.width < 600 &&
 							<span className="account-span">Don't have an Account? <button className="clickLink" onClick={() => { show(!showSignIn) }}>Sign UP</button></span>
 
 						}
 					</form>
-			</div>	</> }
-				{windowSize.width > 600 && <> <div className="overlay-container">
-				  <div className="overlay">
-						<div className="overlay-panel overlay-left">
-						<h1>Welcome Back!</h1>
-						<p>To keep connected with us please login with your personal info</p>
-							<button className="ghost" id="signIn" onClick={() => { props.toggleOverlay('remove') }}>Sign In</button>
-					</div>
-					<div className="overlay-panel overlay-right">
-						<h1>Hello, Friend!</h1>
-						<p>Enter your personal details and start journey with us</p>
-							<button className="ghost" id="signUp" onClick={() => {props.toggleOverlay('add')}}>Sign Up</button>
+					</div>	</>
+				}
+				{windowSize.width > 600 &&
+				    <> <div className="overlay-container">
+				       <div className="overlay">
+							<div className="overlay-panel overlay-left">
+								<h1>Welcome Back!</h1>
+								<p>To keep connected with us please login with your personal info</p>
+								<button className="ghost" id="signIn" onClick={() => { props.toggleOverlay('remove') }}>Sign In</button>
 							</div>
-						
-					</div>
-				</div>
-				 	</>}
+					        <div className="overlay-panel overlay-right">
+								<h1>Hello, Friend!</h1>
+								<p>Enter your personal details and start journey with us</p>
+							    <button className="ghost" id="signUp" onClick={() => {props.toggleOverlay('add')}}>Sign Up</button>
+							</div>	
+					   </div>
+				       </div>
+					</>
+				}
 			
 			</div>
 			</div>
